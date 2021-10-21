8cd3e05547d7882fe903670a579f85965e09e0db
serg-volodeev <serg.volodeev@gmail.com>
2021-10-21 09:15:53 +0300

    comment time added

--- test_items.py
+++ test_items.py
@@ -6,3 +6,6 @@ def test_found_btn_add_to_basket(browser):
     elems = browser.find_elements_by_css_selector(selector)
     assert len(elems) != 0, f"Not found element by css selector '{selector}'"
     assert len(elems) == 1, f"Many elements found by css selector '{selector}'"
+
+    # import time
+    # time.sleep(20)
